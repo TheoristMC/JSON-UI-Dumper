a5f29a08d25eb5658cb006a416d05897a4d5d290
TheoristMC <zunheorist@gmail.com>
2025-10-05 17:52:40 +0800

    Alpha 0.0.04 (Added a rate limit debug)

--- src/main.js
+++ src/main.js
@@ -2,5 +2,17 @@ import "./assets/main.css";
 
 import { createApp } from "vue";
 import App from "./App.vue";
+import { Octokit } from "octokit";
 
 createApp(App).mount("#app");
+
+console.log("Token exist:", !!import.meta.env.VITE_GITHUB_TOKEN);
+
+(async () => {
+  const OctoInit = new Octokit({
+    auth: import.meta.env.VITE_GITHUB_TOKEN,
+  });
+
+  const rate = await OctoInit.request("GET /rate_limit");
+  console.log("Rate limit:", rate.data.rate);
+})();
