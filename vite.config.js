80773732e79c86188cf70d3c90e08aa17b5c6c50
TheoristMC <zunheorist@gmail.com>
2025-10-05 14:50:09 +0800

    Defined the Repo Name

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/repo-name/",
+  base: "/JSON-UI-Dumper/",
   plugins: [
     vue(),
     vueDevTools(),
